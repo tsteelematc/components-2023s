9f492f2a93090044832ca39105a915543b0d9619
Sheldon <kasebrot@live.com>
2023-02-28 21:02:21 -0600

    Added logic for slack-n-tell component.
    Slack-n-tell week 07 finished.

--- src/app/sw-team-check/sw-team-check.component.ts
+++ src/app/sw-team-check/sw-team-check.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sw-team-check',
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class SwTeamCheckComponent {
 
-}
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  teamNames = [
+    "green bay packers"
+    , "packers"
+    , "green bay"
+    , "gbp"
+    , "greenbaypackers"
+  ];
+
+  userInput = "".toLocaleLowerCase();
+
+  result = "";
+
+
+  checkTeamNames = () => {
+
+    const userInputLower = this.userInput.toLocaleLowerCase();
+
+    if (this.teamNames.includes(userInputLower)) {
+      this.result = ("You have chosen wisely! Go Pack Go!");
+    }
+    else {
+      this.result = ("You chose poorly!");
+    }
+
+    this.userInput = "";
+
+    };
+
+  }

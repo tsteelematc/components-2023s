8255879cb8729d76eb96b4d4168b02b5fcb97fa4
Sheldon <kasebrot@live.com>
2023-02-28 20:10:26 -0600

    Generated my team check component...
    Used the command line
    
    ng generate component sw-team-check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { SwTeamCheckComponent } from './sw-team-check/sw-team-check.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    SwTeamCheckComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sw-team-check/sw-team-check.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sw-team-check',
+  templateUrl: './sw-team-check.component.html',
+  styleUrls: ['./sw-team-check.component.css']
+})
+export class SwTeamCheckComponent {
+
+}

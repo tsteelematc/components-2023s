7ba5d60cebd8a4ab52ba7eda8c1c103a81b3f365
Sheldon <kasebrot@live.com>
2023-02-28 20:19:05 -0600

    Starting to build the team check component...
    Remember to import the mat controls I plan to use...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { SwTeamCheckComponent } from './sw-team-check/sw-team-check.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
@@ -25,6 +28,8 @@ import { SwTeamCheckComponent } from './sw-team-check/sw-team-check.component';
     , MatCardModule
     , MatButtonModule
     , MatCheckboxModule
+    , MatInputModule
+    , MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
